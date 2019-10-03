069d362cf3f4989736cf144ad614eb48009ace8b
Lachezar Kutsarov <lkutsarov@gmail.com>
2019-10-03 18:26:10 +0200

    Update Solution.java

--- Solution.java
+++ Solution.java
@@ -16,20 +16,20 @@ public class Soution {
   * Finds the number of long integers with value range 
   * 1 <= (integer value) < (query-1) 
   * that have fulfill the requirement: 
-  * (integer value) XOR (query) < query.
+  * (integer value) XOR (query) > query.
   *
   * The position of the leading '1' in the binary represenation 
-  * in the number that is being checked, is shifted from lower to higher 
-  * bit positions unitl the value of the number is lower than value of the query. 
+  * of the number that is being checked, is shifted from lower to higher 
+  * bit positions unitl the value of this number is lower than value of the query. 
   *
-  * Thus value of the number that is being check is always lower
-  * than the value of query. Therefore, if for the position of the leading '1' 
-  * in the number that is being checked, there is '0' at the same position
-  * in the query, all possible combinations of '0s' and '1s' afterwards fulfill 
-  * the requirement (integer value) XOR (query) < query.
+  * Thus value of the number that is being check is always lower than the value of query. 
+  * Therefore, for the binary represenations of this number and of the query it is always true that:
+  * If for the position of the leading '1' in the number, there is '0' 
+  * at the same position in the query, all possible combinations of '0s' and '1s' 
+  * afterwards fulfill the requirement of (integer value) XOR (query) > query.
   * 
   * @return A long integer representing the total number of integers that fulfill 
-  * the above-mentioned requirements.
+  *         the above-mentioned requirements.
   */
   private static long findGreatXOR(long query) {
     long result = 0;
